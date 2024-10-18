57286801bf5cd6a8d9f2ecde5c8f22243b809608
samuelckraft <samuelckraft@gmail.com>
2024-10-18 14:13:25 -0400

    initialized project and downloaded the following packages: tailwindcss, react-three/fiber, react-three/drei, maath, react-tilt , react-vertical-timeline-component, emailjs/browser, framer-motion, react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
-import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter/>
+  <StrictMode>
     <App />
-    <BrowserRouter/>
-  </React.StrictMode>,
+  </StrictMode>,
 )
